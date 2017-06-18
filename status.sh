06ca52d230a8e8e16cdcf22efb186d93dde5b13b
Divyanshu <divyanshu.1996@gmail.com>
2017-06-18 22:06:28 +0000

    changes made in script

--- status.sh
+++ status.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-printf "Memory\tDisk\tCPU\n"
-while true
-do
-	MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+# printf "Memory\tDisk\tCPU\n"
+# while true
+# do
+	MEMORY=$(free -m | awk 'NR==2{printf "%.2f\t\t", $3*100/$2 }')
 	DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
-	CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
-	echo "$MEMORY$DISK$CPU"
-	sleep 1
-done
+	CPU=$(top -bn1 | grep load | awk '{printf "%.2f\t\t\n", $(NF-2)}')
+	D=$(echo $DISK| cut -d '%' -f 1)
+	# echo $D
+	printf '{"Memory": %s,\n "DISK": %s,\n "CPU": %s}\n' $MEMORY "$D" $CPU
+	# sleep 1
+# done
