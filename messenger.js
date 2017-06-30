1832e175ff7e981daecc82c7ee54025bebb3f24f
Divyanshu <divyanshu.1996@gmail.com>
2017-06-30 15:23:07 +0000

    power cycle module added

--- messenger.js
+++ messenger.js
@@ -379,6 +379,20 @@ module.exports = {
                     }) 
 
                 }
+                else if(text == 4){
+                    sort.states.UserState[sender].stage++;
+                    sort.data[sender] = {type: "power_cycle"};
+                    dof.dropletActions(digitoken, sort.info[sender].id, sort.data[sender], function(body){
+                         console.log(body);
+                        if(body.action==undefined){
+                           
+                            module.exports.sendTextMessage(sender, body.message);
+                        }
+                        else{
+                            module.exports.sendTextMessage(sender, body.action.status);
+                        }
+                    }) 
+                }
             }
         }
 
