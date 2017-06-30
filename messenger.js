61da64f950f2196d457060d672f4f31a709bd469
Divyanshu <divyanshu.1996@gmail.com>
2017-06-30 15:27:59 +0000

    power on module added

--- messenger.js
+++ messenger.js
@@ -421,6 +421,20 @@ module.exports = {
                         }
                     }) 
                 }
+                else if(text == 7){
+                    sort.states.UserState[sender].stage++;
+                    sort.data[sender] = {type: "power_on"};
+                    dof.dropletActions(digitoken, sort.info[sender].id, sort.data[sender], function(body){
+                         console.log(body);
+                        if(body.action==undefined){
+                           
+                            module.exports.sendTextMessage(sender, body.message);
+                        }
+                        else{
+                            module.exports.sendTextMessage(sender, body.action.status);
+                        }
+                    }) 
+                }
             }
         }
 
