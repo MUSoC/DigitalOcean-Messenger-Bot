a404ff0b3ecd68b2b34501f2c10fc2bf1a539e44
Divyanshu <divyanshu.1996@gmail.com>
2017-06-30 15:21:20 +0000

    reboot module added

--- messenger.js
+++ messenger.js
@@ -364,6 +364,21 @@ module.exports = {
                         }
                     })                   
                 }
+                else if(text == 3){
+                    sort.states.UserState[sender].stage++;
+                    sort.data[sender] = {type: "reboot"};
+                    dof.dropletActions(digitoken, sort.info[sender].id, sort.data[sender], function(body){
+                         console.log(body);
+                        if(body.action==undefined){
+                           
+                            module.exports.sendTextMessage(sender, body.message);
+                        }
+                        else{
+                            module.exports.sendTextMessage(sender, body.action.status);
+                        }
+                    }) 
+
+                }
             }
         }
 
